f381106ea8761068a1447c8b5e7aa094ae602778
Duvan Castro <duvancastro312@gmail.com>
2024-10-10 16:01:48 -0500

    ejercicio de vida util de los componentes

---
+++ src/app/domains/info/pages/about/about.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from "@angular/common";
+import { Component, signal } from "@angular/core";
+import { CounterComponent } from "../../../shared/components/counter/counter.component";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [CommonModule,CounterComponent,ReactiveFormsModule],
+  templateUrl: './about.component.html',
+  styleUrls:[ './about.component.css']
+})
+export class AboutComponent {
+  duration = signal(1000);
+  message = signal('Hola');
+
+  changeDuration(event: Event) {
+
+    const input = event.target as HTMLInputElement;
+    console.log('input :>> ', input);
+
+    this.duration.set(input.valueAsNumber) 
+    console.log('duration :>> ', this.duration());
+  }
+
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    console.log('input :>> ', input);
+    this.message.set(input.value) 
+  }
+}
